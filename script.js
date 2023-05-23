659101245d9df0342d422daf178a9c6e2fa3fb47
Erick <erick.nahuelpaiva.001@gmail.com>
2023-05-23 12:07:37 -0300

    implemente vectores

--- script.js
+++ script.js
@@ -14,11 +14,14 @@ const inputUsuario = document.querySelector("#usuario-nombre");
 const inputContrasenia = document.querySelector("#usuario-contrasenia");
 const pResultado = document.querySelector("#resultado");
 
-const USUARIO_NOMBRE_REGISTRADO = "Carlos";
+/* const USUARIO_NOMBRE_REGISTRADO = "Carlos";
 const USUARIO_CONTRASENIA_REGISTRADA = "Carlos1.";
 
 const USUARIO_NOMBRE_REGISTRADO_2 = "Jose";
-const USUARIO_CONTRASENIA_REGISTRADA_2 ="123";
+const USUARIO_CONTRASENIA_REGISTRADA_2 ="123"; */
+
+const usuarioNombRegist = ["Carlos", "Jose"];
+const usuarioContraRegist = ["Carlos1.", "123"];
 
 /* let usuarioNombreIngresado = prompt("Ingrese usuario");
 let usuarioContraseniaIngresada = prompt("Ingresar contraseña");
@@ -30,10 +33,12 @@ let usuarioContraseniaIngresada = "def contrasenia";
 function verificar(){
     usuarioNombreIngresado = inputUsuario.value;
     usuarioContraseniaIngresada = inputContrasenia.value;   
-    if ( ( usuarioContraseniaIngresada == USUARIO_CONTRASENIA_REGISTRADA)  && (usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO)) {
+    if ( ( usuarioContraseniaIngresada == usuarioContraRegist[0])  && (usuarioNombreIngresado == usuarioNombRegist[0])) {
+        pResultado.style.color = "green";
         /* document.write("Usuario y contraseña correctos"); */
         pResultado.innerHTML = "Usuario y contraseña correctos "+usuarioNombreIngresado;
-    } else if(( usuarioContraseniaIngresada == USUARIO_CONTRASENIA_REGISTRADA_2)  && (usuarioNombreIngresado == USUARIO_NOMBRE_REGISTRADO_2)){
+    } else if(( usuarioContraseniaIngresada == usuarioContraRegist[1])  && (usuarioNombreIngresado == usuarioNombRegist[1])){
+        pResultado.style.color = "green";
         pResultado.innerHTML = "Usuario y contraseña correctos "+usuarioNombreIngresado;
     }else {
         pResultado.innerHTML = "Usuario y contraseña incorrectos";
